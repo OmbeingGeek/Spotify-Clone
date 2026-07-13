1d629fec9b52b8653235c41c8289aeec96fe9071
Om <maratheom938@gmail.com>
2026-07-13 09:30:56 +0530

    Add close and hamburger SVG icons; implement toggle functionality in the sidebar

--- script.js
+++ script.js
@@ -106,6 +106,17 @@ async function main() {
         
     })
 
+    document.querySelector(".hamburger").addEventListener("click",()=>{
+        document.querySelector(".left").style.left = "0"
+    })
+    
+    document.querySelector(".close").addEventListener("click",()=>{
+        document.querySelector(".left").style.left = "-100%"
+    })
+
+
+    
+
 
     
 } 
